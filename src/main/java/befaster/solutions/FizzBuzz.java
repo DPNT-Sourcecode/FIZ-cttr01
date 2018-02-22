1edd6e69b512149ca9232a905031cfc21d4c8402
sbx_user1059 <sbx_user1059@ip-10-11-229-26.eu-west-2.compute.internal>
2018-02-22 09:42:11 +0000

    Thu Feb 22 09:42:11 UTC 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,7 +5,20 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        String result = "";
+        if(number % 3 == 0) {
+            result += "fizz";
+        }
+
+        if(number % 5 == 0) {
+            result += " buzz";
+        }
+
+        if(result.isEmpty()) {
+            return number.toString();
+        } else {
+            return result.trim();
+        }
     }
 
 }
--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -1,15 +1,18 @@
 package befaster.solutions;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FizzBuzzTest {
 
     @Test
     public void test_fizz_buzz() {
-        assertThat(FizzBuzz.fizzBuzz(3), equals("fizz"));
-        assertThat(FizzBuzz.fizzBuzz(5), equals("buzz"));
-        assertThat(FizzBuzz.fizzBuzz(15), equals("fizz buzz"));
-        assertThat(FizzBuzz.fizzBuzz(16), equals("16"));
+        assertThat(FizzBuzz.fizzBuzz(3), equalTo("fizz"));
+        assertThat(FizzBuzz.fizzBuzz(5), equalTo("buzz"));
+        assertThat(FizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
+        assertThat(FizzBuzz.fizzBuzz(16), equalTo("16"));
     }
 }
