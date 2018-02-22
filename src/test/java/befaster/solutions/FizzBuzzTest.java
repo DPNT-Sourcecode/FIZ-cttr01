c13cd467a7be60d9a01857303bbc1f9152774766
sbx_user1059 <sbx_user1059@ip-10-11-229-26.eu-west-2.compute.internal>
2018-02-22 09:39:11 +0000

    Thu Feb 22 09:39:11 UTC 2018

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package befaster.solutions;
 
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void test_fizz_buzz() {
+        assertThat(FizzBuzz.fizzBuzz(3), equals("fizz"));
+        assertThat(FizzBuzz.fizzBuzz(5), equals("buzz"));
+        assertThat(FizzBuzz.fizzBuzz(15), equals("fizz buzz"));
+        assertThat(FizzBuzz.fizzBuzz(16), equals("16"));
+    }
 }
