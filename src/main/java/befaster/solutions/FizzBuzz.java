82e11b5e7d07102734bdb2f6b3dfa0a4c145d128
sbx_user1059 <sbx_user1059@ip-10-11-229-26.eu-west-2.compute.internal>
2018-02-22 09:53:25 +0000

    Thu Feb 22 09:53:25 UTC 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -14,10 +14,29 @@ public class FizzBuzz {
             result += " buzz";
         }
 
+        if(number > 10 && hasSameDigits(number)) {
+            result += " deluxe";
+        }
+
         if(result.isEmpty()) {
             return number.toString();
         } else {
             return result.trim();
+        }
+    }
+
+    public static boolean hasSameDigits(Integer x) {
+        String numberString = x.toString();
+        return numberString.replaceAll(numberString.substring(0,1), "").equals("");
+    }
+
+
+    public static void main(String[] args) {
+        for(int i=1; i<1000; i++) {
+            if(hasSameDigits(15 * i)) {
+                System.out.println(15*i);
+                break;
+            }
         }
     }
 
--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -18,5 +18,14 @@ public class FizzBuzzTest {
         assertThat(FizzBuzz.fizzBuzz(137), equalTo("fizz"));
         assertThat(FizzBuzz.fizzBuzz(157), equalTo("buzz"));
         assertThat(FizzBuzz.fizzBuzz(53), equalTo("fizz buzz"));
+
+
+//        - A number is considered to be "deluxe" if it is greater than 10 and all the digits are identical
+//        - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
+
+        assertThat(FizzBuzz.fizzBuzz(7777), equalTo("deluxe"));
+        assertThat(FizzBuzz.fizzBuzz(33), equalTo("fizz deluxe"));
+        assertThat(FizzBuzz.fizzBuzz(5555), equalTo("buzz deluxe"));
+        assertThat(FizzBuzz.fizzBuzz(555), equalTo("fizz buzz deluxe"));
     }
 }
